f8f3385cfc8b7d9904299b39af018fd501d1266c
begnolas <manuel.beniani@gmail.com>
2018-09-19 12:30:23 +0200

    Update alias.bash

--- script/alias.bash
+++ script/alias.bash
@@ -3,7 +3,7 @@
 # Collection of useful alias
 # start this script at the end of .bashrc
 
-alias up='sudo apt-get update; sudo apt-get dist-upgrade -y; sudo apt-get autoclean -y; sudo apt-get autoremove -y'
+alias up='sudo apt update; sudo apt dist-upgrade -y; sudo apt autoclean -y; sudo apt autoremove -y; apt moo'
 
 diskusage () { du -h $1 | sort -rh; }
 diskusageM () { du -h $1 | sort -rh | grep -v '[0-9]K'; }
